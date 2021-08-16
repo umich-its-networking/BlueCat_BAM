7fed97a0e1dbdd9fef47492cd576cded574e5009
Bob Harold <rharolde@umich.edu>
2021-08-16 10:04:36 -0400

    shorten quicktest.sh output

--- quicktest.sh
+++ quicktest.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 files="src/bluecat_bam/*.py tests/*.py samples/*.py *.py"
-echo "To update files with black, use:"
-echo "black $files"
-( set -x
-black --check --diff $files
+#echo "To update files with black, use:"
+#echo "black $files"
+#( set -x
+#black --check --diff $files
+echo "================= black ================"
+black $files
+echo "================= pylint ================" 
 pylint $files
+echo "================= flake8 ================"
 flake8 $files
+echo "================= bandit ================"
 bandit $files
+echo "================= pytest ================"
 pytest
-)
+#)
