ee5672928c61e98c822a57f0d178a2c9bf27c855
Bob Harold <rharolde@umich.edu>
2021-10-28 16:41:21 -0400

    remove unused function

--- samples/get_deployment_options.py
+++ samples/get_deployment_options.py
@@ -79,45 +79,6 @@ def argparsecommon():
     return config
 
 
-def get_network(cidr, configuration_id, conn):
-    """find block or network for a CIDR"""
-    # If both block and network match, return the block
-    # bam getIPRangedByIP containerId=21216763 type=IP4Block address=10.2.1.0
-    ip = cidr.split("/")[0]  # (ip,prefix) = cidr.split("/")
-    block_obj = conn.do(
-        "getIPRangedByIP",
-        method="get",
-        containerId=configuration_id,
-        type="IP4Block",
-        address=ip,
-    )
-    # print("block_obj",json.dumps(block_obj))
-
-    if block_obj["properties"]["CIDR"] == cidr:
-        # print('found matching block',json.dumps(block_obj))
-        entity = block_obj
-    else:
-        # find network
-        network_obj = conn.do(
-            "getIPRangedByIP",
-            method="get",
-            containerId=block_obj["id"],
-            type="IP4Network",
-            address=ip,
-        )
-        network_id = network_obj["id"]
-        # print("existing network",json.dumps(network_obj))
-
-        if network_id == 0:
-            entity = {}
-        elif network_obj["properties"]["CIDR"] == cidr:
-            # print("found matching network",json.dumps(network_obj))
-            entity = network_obj
-        else:
-            entity = {}
-    return entity
-
-
 def main():
     """get_deployment_options"""
     config = argparsecommon()
