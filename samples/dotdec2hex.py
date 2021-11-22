de11b4c97e3eca18ce1af2124817377986f5228b
Bob Harold <rharolde@umich.edu>
2021-11-22 15:34:35 -0500

    dotted decimal to hex utility

---
+++ samples/dotdec2hex.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""dotdec2hex.py [dotted-decimal ...]
+convert dotted decimal to hex with colons
+(use for MAC Addresses)"""
+# translated from Perl /home/jmd/bin/dotdec2hex
+
+import sys
+import re
+import argparse
+
+def main():
+    """CLI for dotdec2hex"""
+    config = argparse.ArgumentParser(
+        description="BlueCat Address Manager add_DNS_Deployment_Role_list"
+    )
+    config.add_argument(
+        "dotdec",
+        nargs="*",
+        help="dotted decimal",
+    )
+
+    args = config.parse_args()
+
+    if args.dotdec:
+        for x in args.dotdec:
+            print(dotdec2hex(x))
+    else:
+        with sys.stdin as f:
+            for line in f:
+                line = re.sub(r'(?:\r\n|\n)$', '', line, count=1)
+                print(dotdec2hex(line))
+
+def dotdec2hex(dotdec):
+    """convert dotted decimal to hex"""
+    hex=[]
+    for decimal in dotdec.split("."):
+        d = int(decimal)
+        if d < 0 or d > 255:
+            print(dotdec,"is not a valid dotted decimal")
+            return None
+        else:
+            h=format(d, "x")
+            hex.append(h)
+    hexout=":".join(hex)
+    return hexout
+
+if __name__ == "__main__":
+    main()
