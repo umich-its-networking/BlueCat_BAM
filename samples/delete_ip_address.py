f13936b6d0a3c6483d90604d972c97519817092d
Bob Harold <rharolde@umich.edu>
2021-12-23 14:18:07 -0500

    first working version

--- samples/delete_ip_address.py
+++ samples/delete_ip_address.py
@@ -11,116 +11,76 @@ DHCP active will be deleted with --force
 # to be python2/3 compatible:
 from __future__ import print_function
 
-import os
-import sys
 import json
-import argparse
 import logging
+import datetime
 
 import bluecat_bam
 
 
-config = argparse.ArgumentParser(
-    description="delete ip address, including linked hostnames"
-)
-config.add_argument("ip", help="IP Address")
-config.add_argument(
-    "--server",
-    "-s",
-    # env_var="BLUECAT_SERVER",
-    default=os.getenv("BLUECAT_SERVER"),
-    help="BlueCat Address Manager hostname",
-)
-config.add_argument(
-    "--username",
-    "-u",
-    # env_var="BLUECAT_USERNAME",
-    default=os.getenv("BLUECAT_USERNAME"),
-)
-config.add_argument(
-    "--password",
-    "-p",
-    # env_var="BLUECAT_PASSWORD",
-    default=os.getenv("BLUECAT_PASSWORD"),
-    help="password in environment, should not be on command line",
-)
-config.add_argument(
-    "--configuration",
-    "--cfg",
-    help="BlueCat Configuration name",
-    default=os.getenv("BLUECAT_CONFIGURATION"),
-)
-config.add_argument("--view", help="BlueCat View", default=os.getenv("BLUECAT_VIEW"))
-config.add_argument(
-    "--logging",
-    "-l",
-    help="log level, default WARNING (30),"
-    + "caution: level DEBUG(10) or less will show the password in the login call",
-    default=os.getenv("BLUECAT_LOGGING", "WARNING"),
-)
-config.add_argument(
-    "--force", "-f", help="delete active and DHCP Reserved also", action="store_true"
-)
-config.add_argument(
-    "--states",
-    help="list of IP states to delete, like --states DHCP_FREE,DHCP_ALLOCATED,DHCP_RESERVED,STATIC"
-    + " (default=all)",
-)
-args = config.parse_args()
+def main():
+    """delete_ip_address"""
+    config = bluecat_bam.BAM.argparsecommon()
+    config.add_argument(
+        "object_ident",
+        help="Can be: entityId (all digits), individual IP Address (n.n.n.n), "
+        + "IP4Network or IP4Block (n.n.n.n/...), or DHCP4Range (n.n.n.n-...).  "
+        + "or a filename or stdin('-') with any of those on each line "
+        + "unless 'type' is set to override the pattern matching",
+    )
+    # config.add_argument("ip", help="IP Address")
+    config.add_argument(
+        "--states",
+        help="list of IP states to delete, like --states"
+        + " DHCP_FREE,DHCP_ALLOCATED,DHCP_RESERVED,STATIC"
+        + " (default=all)",
+    )
+    args = config.parse_args()
 
-logger = logging.getLogger()
-logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
-logger.setLevel(args.logging)
+    logger = logging.getLogger()
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
+    logger.setLevel(args.logging)
 
-configuration_name = args.configuration
-ip = args.ip
-force = args.force
-states = args.states
+    ip = args.object_ident
+    states = args.states
 
-conn = bluecat_bam.BAM(args.server, args.username, args.password)
+    with bluecat_bam.BAM(args.server, args.username, args.password) as conn:
+        (configuration_id, _) = conn.get_config_and_view(args.configuration)
 
-configuration_obj = conn.do(
-    "getEntityByName",
-    method="get",
-    parentId=0,
-    name=configuration_name,
-    type="Configuration",
-)
+        ##input_list = conn.get_obj_list(conn, args.object_ident, configuration_id, "")
 
-configuration_id = configuration_obj["id"]
-
-ip_obj = conn.do(
-    "getIP4Address", method="get", containerId=configuration_id, address=ip
-)
-ip_id = ip_obj["id"]
-
-if ip_id == 0:
-    print("IP Address not found: %s" % (ip))
-else:
-    address = ip_obj["properties"]["address"]
-    state = ip_obj["properties"]["state"]
-    expire = ip_obj["properties"]["expiryTime"]
-    if states and state in states:
-        if state == "DHCP_ALLOCATED":
-            # change to dhcp reserved with a fake mac address, then delete
-            result = conn.do(
-                "changeStateIP4Address",
-                addressId=ip_obj["id"],
-                targetState="MAKE_DHCP_RESERVED",
-                macAddress="deadbeef1234",
-            )
-        result = conn.do(
-            "deleteWithOptions",
-            method="delete",
-            objectId=ip_id,
-            options="noServerUpdate=true|deleteOrphanedIPAddresses=true|",
+        ip_obj = conn.do(
+            "getIP4Address", method="get", containerId=configuration_id, address=ip
         )
-        """check if IP address still exists, should get id=0 if not"""
-        check_ip = conn.do("getEntityById", method="get", id=ip_id)
-        check_ip_id = check_ip["id"]
-        if check_ip_id == 0:
-            print("Deleted IP %s %s" % (address, state))
+        ip_id = ip_obj["id"]
+
+        if ip_id == 0:
+            print("IP Address not found: %s" % (ip))
         else:
-            print("ERROR - IP address failed to delete:", json.dumps(check_ip))
-    else:
-        print("skipped due to state, IP %s, state %s" % (address, state))
+            address = ip_obj["properties"]["address"]
+            state = ip_obj["properties"]["state"]
+            if not states or state in states:
+                if state == "DHCP_ALLOCATED":
+                    # BAM returns seconds with one decimal,
+                    # datetime needs 3, so add a couple zeros
+                    expiretime = datetime.datetime.fromisoformat(
+                        ip_obj["properties"]["expiryTime"] + "00"
+                    )
+                    if expiretime > datetime.datetime.now():
+                        print("warning - dhcp lease time still active", ip_obj)
+                result = conn.delete_ip_obj(ip_obj)
+                if result:
+                    print("delete resulted in", result)
+                # check if IP address still exists, should get id=0 if not
+                check_ip = conn.do("getEntityById", method="get", id=ip_id)
+                check_ip_id = check_ip["id"]
+                if check_ip_id == 0:
+                    print("Deleted IP %s %s" % (address, state))
+                else:
+                    print("ERROR - IP address failed to delete:", json.dumps(check_ip))
+            else:
+                print("skipped due to state, IP %s, state %s" % (address, state))
+
+
+if __name__ == "__main__":
+    main()
