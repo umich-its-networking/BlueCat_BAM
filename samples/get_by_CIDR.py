d729f8a427f6d468cde04f18adb6ae8f423f164b
Bob Harold <rharolde@umich.edu>
2022-03-02 12:27:33 -0500

    formatting

--- samples/get_by_CIDR.py
+++ samples/get_by_CIDR.py
@@ -5,119 +5,80 @@
 # to be python2/3 compatible:
 from __future__ import print_function
 
-import os
-import sys
 import json
-import argparse
 import logging
 import re
 
 import bluecat_bam
 
 
-config = argparse.ArgumentParser(
-    description="get network, block, or both matching the CIDR"
-)
-config.add_argument("cidr", help="CIDR Address/prefix")
-config.add_argument(
-    "--server",
-    "-s",
-    # env_var="BLUECAT_SERVER",
-    default=os.getenv("BLUECAT_SERVER"),
-    help="BlueCat Address Manager hostname",
-)
-config.add_argument(
-    "--username",
-    "-u",
-    # env_var="BLUECAT_USERNAME",
-    default=os.getenv("BLUECAT_USERNAME"),
-)
-config.add_argument(
-    "--password",
-    "-p",
-    # env_var="BLUECAT_PASSWORD",
-    default=os.getenv("BLUECAT_PASSWORD"),
-    help="password in environment, should not be on command line",
-)
-config.add_argument(
-    "--configuration",
-    "--cfg",
-    help="BlueCat Configuration name",
-    default=os.getenv("BLUECAT_CONFIGURATION"),
-)
-config.add_argument("--view", help="BlueCat View", default=os.getenv("BLUECAT_VIEW"))
-config.add_argument(
-    "--logging",
-    "-l",
-    help="log level, default WARNING (30),"
-    + "caution: level DEBUG(10) or less will show the password in the login call",
-    default=os.getenv("BLUECAT_LOGGING", "WARNING"),
-)
-args = config.parse_args()
-
-logger = logging.getLogger()
-logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
-logger.setLevel(args.logging)
-
-configuration_name = args.configuration
-view_name = args.view
-cidr = args.cidr
-
-if not (configuration_name and view_name and cidr):
-    config.print_help()
-    sys.exit(1)
-
-match = re.fullmatch(r"(\d{1,3}\.){3}\d{1,3}/\d{1,2}", cidr)
-if not match:
-    config.print_help()
-    sys.exit(1)
-
-conn = bluecat_bam.BAM(args.server, args.username, args.password)
-
-configuration_obj = conn.do(
-    "getEntityByName",
-    method="get",
-    parentId=0,
-    name=configuration_name,
-    type="Configuration",
-)
-
-configuration_id = configuration_obj["id"]
-logging.info("Configuration id = %s", configuration_id)
-
-ip, prefix = cidr.split("/")
-obj = conn.do(
-    "getIPRangedByIP", method="get", containerId=configuration_id, address=ip, type=""
-)
-obj_id = obj["id"]
-logging.info("getIPRangedByIP obj = %s", json.dumps(obj))
-if obj_id == 0:
-    print("Not found")
-    ranged = False
-else:
-    ranged = True
-
-# bug in BlueCat - if Block and Network have the same CIDR,
-# it should return the Network, but it returns the Block.
-# So check for a matching Network.
-if ranged and obj["type"] == "IP4Block":
-    network_obj = conn.do(
-        "getEntityByCIDR", method="get", cidr=cidr, parentId=obj_id, type="IP4Network"
+def main():
+    """get network or block matching CIDR"""
+    config = bluecat_bam.BAM.argparsecommon("get network and/or block matching CIDR")
+    config.add_argument("cidr", help="CIDR Address/prefix")
+    config.add_argument(
+        "--type", help="optional type: IP4Block or IP4Network, defaults to both"
     )
-    if network_obj["id"]:
-        obj = network_obj
+    args = config.parse_args()
+    cidr = args.cidr
+    range_type = args.type
 
-while ranged:  # always True or always False, this is a loop - until
-    # network and block have CIDR, DHCP range does not
-    found_cidr = obj["properties"].get("CIDR")
-    if found_cidr:
-        found_ip, found_prefix = found_cidr.split("/")
-        if found_ip == ip and found_prefix == prefix:
-            print(json.dumps(obj))
-        if found_ip != ip or int(found_prefix) < int(prefix):
-            break
-    # walk up the tree
-    obj = conn.do("getParent", method="get", entityId=obj["id"])
-    logging.info("parent obj = %s", json.dumps(obj))
+    logger = logging.getLogger()
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
+    logger.setLevel(args.logging)
 
-# print(json.dumps(ip_obj))
+    with bluecat_bam.BAM(args.server, args.username, args.password) as conn:
+        (cfg_id, _) = conn.get_config_and_view(args.configuration, None)
+        match = re.fullmatch(r"(\d{1,3}\.){3}\d{1,3}/\d{1,2}", cidr)
+        if not match:
+            print("cidr not found in", cidr)
+            return
+        ip, prefix = cidr.split("/")
+        obj = conn.do(
+            "getIPRangedByIP",
+            method="get",
+            containerId=cfg_id,
+            address=ip,
+            type=range_type,
+        )
+        logging.info("getIPRangedByIP obj = %s", json.dumps(obj))
+        if not obj or obj["id"] == 0:
+            print("Not found")
+            return
+
+        # bug in BlueCat - if Block and Network have the same CIDR,
+        # it should return the Network, but it returns the Block.
+        # So check for a matching Network.
+        if not range_type and obj["type"] == "IP4Block":
+            network_obj = conn.do(
+                "getEntityByCIDR",
+                method="get",
+                cidr=cidr,
+                parentId=obj["id"],
+                type="IP4Network",
+            )
+            if network_obj["id"]:
+                obj = network_obj
+
+        while True:  # this is a "loop - until"
+            # network and block have CIDR, DHCP range does not
+            found_cidr = obj["properties"].get("CIDR")
+            if found_cidr:
+                found_ip, found_prefix = found_cidr.split("/")
+                if (
+                    found_ip == ip
+                    and found_prefix == prefix
+                    and range_type in (None, obj["type"])
+                ):
+                    print(json.dumps(obj))
+                if found_ip != ip or int(found_prefix) < int(prefix):
+                    break
+            # walk up the tree
+            obj = conn.do("getParent", method="get", entityId=obj["id"])
+            logging.info("parent obj = %s", json.dumps(obj))
+
+        # print(json.dumps(ip_obj))
+
+
+if __name__ == "__main__":
+    main()
