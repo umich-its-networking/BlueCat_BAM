dcb452de1d9d9f7ecab475ae3a971246754d804a
Bob Harold <rharolde@umich.edu>
2022-04-25 14:57:25 -0400

    update to use new functions

--- samples/add_user.py
+++ samples/add_user.py
@@ -12,7 +12,6 @@ import logging
 
 import bluecat_bam
 
-
 # ###### PRESET CONSTANTS - EDIT FOR YOUR USE ######
 # preset constants (edit for your use)
 authenticator_id = 9115247  # should look up authenticator, but this is faster
@@ -20,65 +19,47 @@ password = "this_password_is_not_used"  # nosec  - dummy password required,
 #                                       but not used by authenticator
 # ###### there are other assumptions in this code - review before using ######
 
-config = argparse.ArgumentParser(description="BlueCat Address Manager add user")
-config.add_argument(
-    "--server",
-    "-s",
-    # env_var="BLUECAT_SERVER",
-    default=os.getenv("BLUECAT_SERVER"),
-    help="BlueCat Address Manager hostname",
-)
-config.add_argument(
-    "--username",
-    "-u",
-    # env_var="BLUECAT_USERNAME",
-    default=os.getenv("BLUECAT_USERNAME"),
-)
-config.add_argument(
-    "--password",
-    "-p",
-    # env_var="BLUECAT_PASSWORD",
-    default=os.getenv("BLUECAT_PASSWORD"),
-    help="password in environment, should not be on command line",
-)
-config.add_argument("newusername")
-config.add_argument("newfirstname")
-config.add_argument("newlastname")
-config.add_argument("newemail")
-config.add_argument(
-    "--logging",
-    "-l",
-    help="log level, default WARNING (30),"
-    + "caution: level DEBUG(10) or less will show the password in the login call",
-    default=os.getenv("BLUECAT_LOGGING", "WARNING"),
-)
-args = config.parse_args()
+def main():
+    """add user""
+    config = bluecat_bam.BAM.argparsecommon(
+        "BlueCat Address Manager add user"
+    )
+    config.add_argument("newusername")
+    config.add_argument("newfirstname")
+    config.add_argument("newlastname")
+    config.add_argument("newemail")
 
-logger = logging.getLogger()
-logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
-logger.setLevel(args.logging)
+    args = config.parse_args()
 
-conn = bluecat_bam.BAM(args.server, args.username, args.password)
+    logger = logging.getLogger()
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
+    logger.setLevel(args.logging)
 
-user_obj_id = conn.do(
-    "addUser",
-    method="post",
-    username=args.newusername,
-    password=password,
-    properties={
-        "email": args.newemail,
-        "firstname": args.newfirstname,
-        "lastname": args.newlastname,
-        "authenticator": authenticator_id,
-        "userType": "REGULAR",
-        "securityPrivilege": "VIEW_OTHERS_ACCESS_RIGHTS",  # our default
-        "userAccessType": "GUI",
-        "historyPrivilege": "VIEW_HISTORY_LIST",  # our default
-    },
-)
+    configuration_name = args.configuration
 
-user_obj = conn.do("getEntityById", method="get", id=user_obj_id)
+    with bluecat_bam.BAM(args.server, args.username, args.password) as conn:
+        (configuration_id, _) = conn.get_config_and_view(configuration_name)
 
-print(json.dumps(user_obj))
+        user_obj_id = conn.do(
+            "addUser",
+            method="post",
+            username=args.newusername,
+            password=password,
+            properties={
+                "email": args.newemail,
+                "firstname": args.newfirstname,
+                "lastname": args.newlastname,
+                "authenticator": authenticator_id,
+                "userType": "REGULAR",
+                "securityPrivilege": "VIEW_OTHERS_ACCESS_RIGHTS",  # our default
+                "userAccessType": "GUI",
+                "historyPrivilege": "VIEW_HISTORY_LIST",  # our default
+            },
+        )
 
-conn.logout()
+        user_obj = conn.do("getEntityById", method="get", id=user_obj_id)
+
+        print(json.dumps(user_obj))
+
+if __name__ == "__main__":
+    main()
