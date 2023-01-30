cb8fd362924803c2cb26ffdd198a9d257b06cb75
Bob Harold <rharolde@umich.edu>
2023-01-30 16:23:55 -0500

    update to newer conventions

--- samples/get_serverInterfaceId.py
+++ samples/get_serverInterfaceId.py
@@ -1,71 +1,23 @@
 #!/usr/bin/env python
 
-"""get_serverInterfaceId.py BDDSservername --view view --cfg configuration"""
+"""get_serverInterfaceId.py BDDSservername"""
 
 # to be python2/3 compatible:
 from __future__ import print_function
 
-import os
-import argparse
 import logging
 
 import bluecat_bam
 
 
 __progname__ = "get_serverInterfaceId"
-__version__ = "0.1"
+__version__ = "0.2"
 
 
 def main():
     """get server interface id"""
-    config = argparse.ArgumentParser(
-        description="BlueCat Address Manager get_serverInterfaceId"
-    )
-    config.add_argument(
-        "--server",
-        "-s",
-        # env_var="BLUECAT_SERVER",
-        default=os.getenv("BLUECAT_SERVER"),
-        help="BlueCat Address Manager hostname",
-    )
-    config.add_argument(
-        "--username",
-        "-u",
-        # env_var="BLUECAT_USERNAME",
-        default=os.getenv("BLUECAT_USERNAME"),
-    )
-    config.add_argument(
-        "--password",
-        "-p",
-        # env_var="BLUECAT_PASSWORD",
-        default=os.getenv("BLUECAT_PASSWORD"),
-        help="password in environment, should not be on command line",
-    )
-    config.add_argument(
-        "--configuration",
-        "--cfg",
-        help="BlueCat Configuration name",
-        default=os.getenv("BLUECAT_CONFIGURATION"),
-    )
-    config.add_argument(
-        "--raw",
-        "-r",
-        default=os.getenv("BLUECAT_RAW"),
-        help="set to true to not convert strings like 'name=value|...' "
-        + "to dictionaries on output.  Will accept either format on input.",
-    )
-    config.add_argument(
-        "--version", action="version", version=__progname__ + ".py " + __version__
-    )
-    config.add_argument(
-        "--logging",
-        "-l",
-        help="log level, default WARNING (30),"
-        + "caution: level DEBUG(10) or less will show the password in the login call",
-        default=os.getenv("BLUECAT_LOGGING", "WARNING"),
-    )
+    config = bluecat_bam.BAM.argparsecommon("BlueCat Address Manager get_serverInterfaceId")
     config.add_argument("bdds", help="BDDS server name")
-
     args = config.parse_args()
 
     logger = logging.getLogger()
